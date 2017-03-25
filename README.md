cc2b43bed2ade196cdae384290538e0ea3aeb2d4
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2017-03-24 17:38:48 -0700

    Improving configuration script, and updating README.md.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 lgtinit
 -------
 
-Initializes project scaffolding for logtalk.
+`lgtinit` is a script that initializes basic logtalk project scaffolding from some template files.
+
+To install `lgtinit` please run:
+```
+$ ./configure --prefix=PREFIX_PATH
+$ make install
+```
+
+Run `make install` with the appropriate permissions for your the specified path. Template files will
+be installed in the `share` directory of `PREFIX_PATH`, and the executable will be installed to
+the `bin` directory of `PREFIX_PATH`.
--- configure.sh
+++ configure.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-for arg in "$@"; do
-	case "$arg" in
-		--prefix=*)
-		PREFIX=`echo $arg | sed 's/--prefix=//'`
-		echo "PREFIX=$PREFIX" > Makefile
-		echo -e "#!/bin/bash\n\nPREFIX=$PREFIX" > lgtinit.sh
-		cat Makefile.in >> Makefile
-		cat lgtinit.in >> lgtinit.sh
-		chmod a+x lgtinit.sh
-		;;
-	esac
-done
+if [ -n "$2" ]; then
+	echo "Configure only accepts --prefix argument."
+	exit 1
+fi
+
+case "$1" in
+	--prefix=*)
+	PREFIX=`echo $1 | sed 's/--prefix=//'`
+	echo "PREFIX=$PREFIX" > Makefile
+	echo -e "#!/bin/bash\n\nPREFIX=$PREFIX" > lgtinit.sh
+	cat Makefile.in >> Makefile
+	cat lgtinit.in >> lgtinit.sh
+	chmod a+x lgtinit.sh
+	;;
+
+	*)
+	if [ -n "$1" ]; then
+		echo "Configure only accepts --prefix argument."
+		exit 1
+	fi
+	;;
+esac
 
 if [ -z "$PREFIX" ] ; then
 	PREFIX=/usr/local
